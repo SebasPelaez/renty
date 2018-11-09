ea0858ecb84e8d2643d510714b49c9e4e9a9f115
Sebas <sebaspvilla@hotmail.com>
2018-11-08 21:05:34 -0500

    Changes in style

--- src/containers/SignIn.js
+++ src/containers/SignIn.js
@@ -10,18 +10,33 @@ const SignIn = ({ authError, signIn }) =>  {
   return (
     <Popup trigger={ algo } position="right center" modal>
       <React.Fragment>
-        <div>
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit"
-            onClick={ () => signIn('GOOGLE') }>
-            Login With Google
-          </button>
-          
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit"
-            onClick={ () => signIn('FACEBOOK') }>
-            Login With Facebook
-          </button>
+        <div className="login-container">
+          <div className = "login-header">
+            <div className = "login-header-logo">
+              <h1>Renty</h1>
+            </div>
+            <div className = "login-header-text">
+              <h2> ¡Hola! </h2>
+              <p> Inicia sesión para recibir ofertas y ver los detalles de tu carro.</p>
+            </div>
+          </div>
+          <div className = "login-action">
+            <h3> Puedes hacerlo con </h3>
+            <button className="google-btn" type="submit"
+              onClick={ () => signIn('GOOGLE') }>
+              <div className = "google-logo">                
+              </div>
+              <p>Login With Google</p>
+            </button>
+            <p> ó </p>
+            <button className="fb-btn" type="submit"
+              onClick={ () => signIn('FACEBOOK') }>
+              <div className = "fb-logo">                
+              </div>
+              <p>Login With Facebook</p>
+            </button>
+            </div>          
         </div>
-
         <div>
           {authError && <p>{authError}</p>}
         </div>
