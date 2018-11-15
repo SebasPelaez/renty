e18f0c28738bcaadf9db074acb744af1899f2833
Sebas <sebaspvilla@hotmail.com>
2018-11-14 20:29:26 -0500

    Se actualizó el nombre del objeto que maneja el botón del popup, Se identaron correctamente las líneas, el estilo de los botones fue organizado correctamente

--- src/containers/SignIn.js
+++ src/containers/SignIn.js
@@ -6,31 +6,32 @@ import './SignIn.scss'
 
 const SignIn = ({ authError, signIn }) => {
 
-  const algo = <button className="btn btn-outline-success my-2 my-sm-0" type="submit"> Login </button>
+  const popupTrigger = <button className="btn btn-outline-success my-2 my-sm-0" type="submit"> Login </button>
 
   return (
-    <Popup trigger={algo} position="right center" modal>
+    <Popup trigger={popupTrigger} position="right center" modal>
       <React.Fragment>
         <div className="login-container">
           <div className="login-header">
             <div className="login-header-logo">
               <h1>Renty</h1>
             </div>
-            <div class="login-header-text">
+            <div className="login-header-text">
               <p> Hola! Inicia sesión para recibir ofertas y ver los detalles de tu carro.</p>
             </div>
           </div>
           <div className="login-action">
             <hr></hr>
             <h4> Puedes hacerlo con </h4>
-            <button className="google-btn" type="submit"
+            <button className="social-btn" type="submit"
               onClick={() => signIn('GOOGLE')}>
-              <img width="100%" src="https://drive.google.com/uc?id=1aqngPzzyAAI9HlQd3RtgzBPUc-2z4nSI" alt=""></img>
+              <img src="https://drive.google.com/uc?id=11AzsJsivKsIZ6JM2xf1z9C0pbOPeuhyn" className="fb-logo" alt=""></img>
+              <p>Login With Google</p>
             </button>
             <p> ó </p>
-            <button className="fb-btn" type="submit"
+            <button className="social-btn" type="submit"
               onClick={() => signIn('FACEBOOK')}>
-              <img src="https://drive.google.com/uc?id=11AzsJsivKsIZ6JM2xf1z9C0pbOPeuhyn" class="fb-logo" alt=""></img>
+              <img src="https://drive.google.com/uc?id=11AzsJsivKsIZ6JM2xf1z9C0pbOPeuhyn" className="fb-logo" alt=""></img>
               <p>Login With Facebook</p>
             </button>
           </div>          
@@ -40,15 +41,15 @@ const SignIn = ({ authError, signIn }) => {
         </div>
       </React.Fragment>
     </Popup>
-          )
+  )
 }
 
 const mapStateToProps = state => ({
-            authError: state.auth.authError
+  authError: state.auth.authError
 })
 
 const mapDispatchToProps = dispatch => ({
-            signIn: type => dispatch(signIn(type))
+  signIn: type => dispatch(signIn(type))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(SignIn)
