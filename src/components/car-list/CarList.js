9cbd09f8846886534f195e2c9f99221a60393217
juan_emg <esteban.marin@xpertgroup.co>
2018-11-15 14:30:32 -0500

    configuration of card item style and map of properties respectly

--- src/components/car-list/CarList.js
+++ src/components/car-list/CarList.js
@@ -1,7 +1,53 @@
 import React from 'react'
 import Car from './Car'
+import './Car.scss'
 
 const CarList = ({ cars, isFetching }) => {
+    cars = [{
+        'id':1,
+        'brand':"Toyota",
+        'thumbnail':"http://static.carroya.com/vehiculos/1798530/1798530_1_w.jpg",
+        'price':30000,
+        'type': "pick-up",
+        'model':"2015",
+        'rental':{'id':1,'name':"Renta autos"}
+    },
+    {
+        'id':2,
+        'brand':"Toyota",
+        'thumbnail':"http://static.carroya.com/vehiculos/1798530/1798530_1_w.jpg",
+        'price':30000,
+        'type': "pick-up",
+        'model':"2015",
+        'rental':{'id':1,'name':"Renta autos"}
+    },
+    {
+        'id':3,
+        'brand':"Toyota",
+        'thumbnail':"http://static.carroya.com/vehiculos/1798530/1798530_1_w.jpg",
+        'price':30000,
+        'type': "pick-up",
+        'model':"2015",
+        'rental':{'id':1,'name':"Renta autos"}
+    },
+    {
+        'id':4,
+        'brand':"Toyota",
+        'thumbnail':"http://static.carroya.com/vehiculos/1798530/1798530_1_w.jpg",
+        'price':30000,
+        'type': "pick-up",
+        'model':"2015",
+        'rental':{'id':1,'name':"Renta autos"}
+    },
+    {
+        'id':5,
+        'brand':"Toyota",
+        'thumbnail':"http://static.carroya.com/vehiculos/1798530/1798530_1_w.jpg",
+        'price':30000,
+        'type': "pick-up",
+        'model':"2015",
+        'rental':{'id':1,'name':"Renta autos"}
+    }];
   if (isFetching) {
     return <span> cargando...</span>
   } else if (cars.length === 0) {
@@ -15,16 +61,16 @@ const CarList = ({ cars, isFetching }) => {
     )
   } else {
     return(
-      <ul className="car-list">
+    <div className="container-flex">
         {cars.map(car => 
-          <Car
+            <Car
             key={car.id}
             {...car}
           />
         )}
-      </ul>
+        </div>
     )
   }
 }
 
-export default ProductList
+export default CarList
