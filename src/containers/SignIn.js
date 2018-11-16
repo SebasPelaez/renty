84f3f6b5a897a8074086fb6e632f442f8391a83b
Juan Camilo Morales <camov1209@gmail.com>
2018-11-16 00:18:33 -0500

    Improves sign-in popup style

--- src/containers/SignIn.js
+++ src/containers/SignIn.js
@@ -3,6 +3,8 @@ import Popup from 'reactjs-popup'
 import { connect } from 'react-redux'
 import { signIn } from '../actions/auth'
 import './SignIn.scss'
+import facebookLogo from '../assets/fb.png'
+import googleLogo from '../assets/goo.png'
 
 const SignIn = ({ authError, signIn }) => {
 
@@ -10,36 +12,28 @@ const SignIn = ({ authError, signIn }) => {
 
   return (
     <Popup trigger={popupTrigger} position="right center" modal>
-      <React.Fragment>
-        <div className="login-container">
-          <div className="login-header">
-            <div className="login-header-logo">
-              <h1>Renty</h1>
-            </div>
-            <div className="login-header-text">
-              <p> Hola! Inicia sesión para recibir ofertas y ver los detalles de tu carro.</p>
-            </div>
-          </div>
-          <div className="login-action">
-            <hr />
-            <h4> Puedes hacerlo con </h4>
-            <button className="social-btn" type="submit"
-              onClick={() => signIn('GOOGLE')}>
-              <img src="https://drive.google.com/uc?id=11AzsJsivKsIZ6JM2xf1z9C0pbOPeuhyn" className="fb-logo" alt=""></img>
-              <p>Login With Google</p>
-            </button>
-            <p> ó </p>
-            <button className="social-btn" type="submit"
-              onClick={() => signIn('FACEBOOK')}>
-              <img src="https://drive.google.com/uc?id=11AzsJsivKsIZ6JM2xf1z9C0pbOPeuhyn" className="fb-logo" alt=""></img>
-              <p>Login With Facebook</p>
-            </button>
-          </div>          
+      <div className="popup-wrapper">
+        <h4>We are <strong>Renty</strong></h4>
+        <p className="mb-0 mt-3">
+          Welcome Back! Please login to your account to get another adventure on the road.
+        </p>
+
+        <div className="auth-options">
+          <button className="btn btn-social" type="button" onClick={() => signIn('GOOGLE')}>
+            <img src={googleLogo} alt="" />
+            Continue with Google
+          </button>
+
+          <button className="btn btn-social" type="button" onClick={() => signIn('FACEBOOK')}>
+            <img src={facebookLogo} alt=""/>
+            Continue with Facebook
+          </button>
         </div>
+
         <div>
           {authError && <p>{authError}</p>}
-        </div>
-      </React.Fragment>
+        </div>  
+      </div>
     </Popup>
   )
 }
--- src/containers/header/SignedInLinks.js
+++ src/containers/header/SignedInLinks.js
@@ -2,21 +2,15 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { signOut } from '../../actions/auth'
 
-const SignedInLinks = (props) => {
+const SignedInLinks = ({ dispatch }) => {
   return (
     <div>
       <button className="btn btn-outline-success my-2 my-sm-0" type="submit"
-        onClick={props.signOut}>
+        onClick={ () => dispatch(signOut())}>
         Log Out
       </button>
     </div>
   )
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    signOut: () => dispatch(signOut())
-  }
-}
-
-export default connect(null, mapDispatchToProps)(SignedInLinks)
+export default connect()(SignedInLinks)
