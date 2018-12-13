12b3fdc2365fa4812e1f6b7759bdce760afa1e9a
juan_emg <esteban.marin@xpertgroup.co>
2018-12-13 00:31:46 -0500

    Refactor the cars detail  modal

--- src/components/carList/Car.js
+++ src/components/carList/Car.js
@@ -1,48 +1,61 @@
 import React from 'react'
-import Popup from 'reactjs-popup'
+import Modal from 'react-responsive-modal';
 import './Car.scss'
 import RentalCarDetails from '../../containers/RentalCarDetails'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const Car = props => {
 
-  const popupTrigger = <div className="view-details-wrapper">
-      <button className="view-details">
-        VIEW DETAILS
-      </button>
-    </div>
-  const settingsModal = {
-    width: "80% !important"
+class Car extends React.Component{
+  constructor(props){
+    super(props)
   }
 
-  return(
-    <div className="rental-car card-item shadow-sm">
+  state = {
+    open: false,
+  };
+  
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
+
+  render(){
+    const { open } = this.state;
+
+    return(
+      <div className="rental-car card-item shadow-sm">
       <figure>
-        <img src={props.thumbnail} alt={`Car #${props.id}`}/>
+        <img src={this.props.thumbnail} alt={`Car #${this.props.id}`}/>
       </figure>
 
       <div className="content">
         <p className="title mb-2 truncated">
-          {props.brand} {props.model}
+          {this.props.brand} {this.props.model}
         </p>
 
         <div>
           <FontAwesomeIcon icon='car' className="mr-2" fixedWidth />
-          {props.type}
+          {this.props.type}
         </div>
 
         <div className="font-weight-bold my-4 price">
           <span className="mr-1 currency">COP$</span>
-          <span className="amount">{props.price.toLocaleString()}</span>
+          <span className="amount">{this.props.price.toLocaleString()}</span>
         </div>
-
-        <Popup trigger={popupTrigger} position="right center" modal contentStyle={settingsModal}>          
-          <RentalCarDetails carId={props.id} rentalId={props.rental.id} />
-        </Popup>
+        <div className="view-details-wrapper">
+          <button onClick={this.onOpenModal} className="view-details"> VIEW DETAILS </button>
+        </div>
+        <Modal styles={{padding:0}} open={open} onClose={this.onCloseModal} center>
+          <RentalCarDetails carId={this.props.id} rentalId={this.props.rental.id} />
+        </Modal>
 
       </div>
     </div>
-  )
+    )
+  }
 }
 
 export default Car
