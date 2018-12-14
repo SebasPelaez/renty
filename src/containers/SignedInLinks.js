7abf85f71d2317745b202e15203e31f3d744af6d
Juan Camilo Morales <camov1209@gmail.com>
2018-12-13 21:53:49 -0500

    Fixes errors

--- src/containers/SignedInLinks.js
+++ src/containers/SignedInLinks.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { signOut } from "../actions/auth";
+import { Link } from "react-router-dom";
 import avatar from "../assets/avatar.png";
 import "../index.scss";
 import ReactTooltip from "react-tooltip";
